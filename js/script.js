fc2868008203863743a5b8da90d146a650fbe05c
User w071 <usw071@CENTRAL.GNT.GR>
2017-01-30 12:23:47 +0200

    codenamegame

---
+++ js/script.js
@@ -0,0 +1,85 @@
+var configurationsR = [["BBBRB","YRRYY","YDBRY","RRBYB","RRRYB"],["BBBRR","RDYRR","BYYYB","YBRYB","RBRRY"],["BBDRY","BRBRB","RRYYR","YRBRR","BYYYB"],["BBRRR","BYRYB","RYBRB","YBYRB","DRRYY"],["BBYRY","BRRRY","RBRBR","YRYBY","BRDBY"],["BBYYB","RRBRY","RBBRB","YDYYR","RYRBR"],["BDBBY","YRRRR","RBRRY","YBYYB","BYBRR"],["BRBBR","RRYRY","YYRBB","BYBYR","YRRBD"],["BRBYB","YBBRR","RRYBB","BYRRY","YDRRY"],["BRBYR","YRBBB","RYDRY","BYRRB","YRYRB"],["BRBYY","YRBRR","BBYRR","RBRYD","BYRBY"],["BRDYR","YRBRY","YRBBY","BBRYB","RYBRR"],["BRRYR","YBRBR","BBRYY","BYRYB","RDYBR"],["BRYBR","YRYRR","BBYDY","RRBYB","YRBBR"],["BRYRY","BRRYB","YRDYR","BBBRY","RYBRB"],["BYBRR","YRRYB","YBBYR","BRBDY","BRRYR"],["BYBRY","BBYRY","RBRRR","RBYBB","YRYDR"],["BYRBB","RRBRB","DYRRY","BBBRR","YYRYY"],["BYRBB","YRRRB","YBYBD","YRYRR","BRRBY"],["BYRBR","DBBBR","YRRYR","RBYBR","YBYRY"],["BYRRR","BYBRR","YRBDY","YYRRY","BRBBB"],["BYRYB","YBBRD","BYRRB","RYRRB","RBYRY"],["BYYBB","RYRYY","BRBBR","BRDRR","BYYRR"],["BYYRR","RYDBY","RBBYB","RYRBR","RBYBR"],["BYYYB","RRBRY","RYYRR","BRBRB","YRDBB"],["DBBYY","BBBRR","RRYRR","YBYBY","YRRBR"],["DBRRY","RYBYB","BBRYY","YRYRR","RBBRB"],["DYRBB","RBYYB","RYBRR","YRRYR","YBBBR"],["RBBBY","RYRRY","RRBYR","BYYBR","BBRYD"],["RBBRY","BYBRR","YDYBB","RRYRY","RBYRB"],["RBBYB","DYBBR","YRRRR","BYYBY","RBYRR"],["RBRBY","RDBBB","BYRRR","YBBYR","YYRYR"],["RBRRY","YBYBY","RRYRR","RRBBB","YYBBD"],["RBRYR","RYYDY","BRBBR","YRBRR","BYYBB"],["RBYBB","YBRRR","BBDRY","RRYYR","BYRBY"],["RBYBR","RBRYR","BYBBR","YBDYR","RRYYB"],["RBYDR","BYRYB","YYRBB","RBRBY","RYRRB"],["RBYYB","YBRRR","BRBBD","RYBRY","RBYYR"],["RDYRR","RYRBR","BBBRB","RYRYB","YBYBY"],["RDYRY","BBYBR","RRRBR","YRYBB","YRBYB"],["RRBBY","RBRYB","YRBRY","DYBYB","RRBRY"],["RRBBY","RRYYR","BYYRR","BDYBB","BRBYR"],["RRBYB","BYYBY","YRRBR","RRRRY","YBBDB"],["RRBYR","BYRBB","YBBRY","YRBRY","RYDRB"],["RRRRB","BYBYY","YRBDY","BBYBR","RRBYR"],["RRRRY","BBYBR","RBRYY","YBRBB","BDYRY"],["RRRYY","BYYBR","BRDBB","YRRBY","RBBRY"],["RRYBR","BRDYB","YYYBB","RRBRR","BYBRY"],["RRYBR","YBYYB","RRRRY","RBBYD","BYBBR"],["RRYYB","RRDRB","RBBRB","YYRYR","BBYYB"],["RYBBR","BRRYR","BRDYR","RBBBY","YYBRY"],["RYBRB","BBYDB","RRYYB","RYYRR","YBBRR"],["RYBRD","BRBYB","BYRBR","RRYYR","BRYBY"],["RYBRR","RBYBB","YDBRY","YYBYB","BRRRR"],["RYRRB","YDBRB","RYBBY","BYRRY","RRBYB"],["RYRYY","RYBBY","RRRYB","BBBDR","YBRBR"],["RYYBR","YRBYR","DBBRB","RRRBY","BYYBR"],["YBDRB","YBYRY","RBRBR","YRRRB","YRYBB"],["YBRRB","RRYRY","DBYBY","BRRRY","BBRYB"],["YBRRR","BBRYY","RBRBR","BDYBY","RRBYY"],["YBRYB","DYRBR","RRYBB","RRBRY","YYBRB"],["YBRYB","RYYRR","YRDBB","RRRBY","BBYBR"],["YBYBY","BYRYR","BRBBB","RBRYR","RRYDR"],["YBYRB","RYYRR","RBRYB","BYBRB","DRBYR"],["YRBBR","YBRRY","BBDRB","RBYYB","YYRRR"],["YRBRR","BYBYD","YRBRY","BYRBR","YBBRR"],["YRBYB","RRBRR","BBYYY","BYDRB","RBYRR"],["YRBYY","YBBBR","RYDRB","RYRRB","RBBYR"],["YRRBB","RBBBY","YYRYB","DBRRR","RBRYY"],["YRRBR","BYRBY","BYYYB","RRYDR","RRBBB"],["YRRDY","YRRYB","BBYRR","RRBBY","BYBRB"],["YRRYR","YRRBB","BBYYR","BBRBR","DBRYY"],["YRYBR","BRRYR","BRRYB","DRBBY","BYRYB"],["YRYBY","RBYBR","RYRRY","RBBBD","RBRYB"],["YRYDB","BBRBY","YYRBR","RBYBB","YRRRR"],["YYBRR","YBYDB","RBRBR","YYRBB","RRRBY"],["YYRBD","RBRBB","RYYBB","BBRRY","RYRRY"],["YYRBR","RRRBD","BYRYY","YBBBR","BBRRY"],["YYRRD","BRYBY","BRBYR","BYRYB","RRRBB"],["YYRYY","RRBBB","YRRYD","BRBRR","BBRYB"]];
+var configurationsB = [["BBBRY","RYRRR","YBYYB","BRYRD","BBYBR"],["BBYRB","BRBYB","YYYRB","BRYRR","RDBRY"],["BBYRB","YRBBY","BYDRB","RBYRR","YRYBR"],["BBYRY","RBBYB","RBRBY","RRBRY","BYDYR"],["BBYYY","BBRRY","YDBRB","RRYRB","RBRYB"],["BRBBY","BYBYR","BDRRY","RYRBY","YBRBR"],["BRBYB","RRYBR","RBRYR","YBYBB","DYYRB"],["BRBYD","YYRRY","RBBBR","RYRYB","BRBBY"],["BRBYR","RYYDB","BBBBY","YRYBR","RRRYB"],["BRDBY","YYRRB","YBRRB","BYRBY","RBBRY"],["BRDYY","BRBBR","BBYYR","YBRYB","RRYBR"],["BRRBB","YBYBR","BYRYY","RRBBY","BRRYD"],["BRRRB","YRBBB","DBRBY","YRBYR","RYYBY"],["BRRRY","BRYYB","YBBYR","RYBDB","BRYRB"],["BRRRY","YYBRB","YBYBR","BRRBB","DYBYR"],["BRRYB","RYBYR","BRBRB","BYBRY","YBRYD"],["BRYBB","RYBRR","YBBYR","RDYYR","BBRBY"],["BRYRB","BDBYR","RYBBY","BYYRB","YRRRB"],["BRYYD","BBYBY","RYRBR","RBYRR","BYBRB"],["BYBRR","RBRRB","YBDYY","BRYBR","BYBRY"],["BYBRY","RYBBD","RRBYR","BBYBR","RYRYB"],["BYBYR","RBRBB","YYDRY","RBRRB","RBYBY"],["BYRBR","BRYRR","BRBDY","YRRBB","YYYBB"],["BYRBY","BBYYD","BBRBR","YRRBR","RYRBY"],["BYRBY","RRBYD","RYRRB","BBBBY","YBRYR"],["BYRRR","RBYRY","YBBBB","BDYYR","RYBRB"],["BYRYR","RBYBB","RYBRR","DBBYR","YRBYB"],["BYYBY","BRRBB","BRYBR","BRYDR","RYRBY"],["BYYRR","BYBBB","RDYRB","RBRBY","RYYBR"],["DBYYB","YRBYR","BRYBR","YBBRR","RBRBY"],["DYRBY","YRBYB","BRBRB","RYBYR","BYRRB"],["DYRRB","YBBRR","YYRYB","RBYBY","BBRRB"],["RBBBB","YRRRY","RYYRY","BDBBB","YRRBY"],["RBBBR","DYBRR","BRBYB","RYYYR","YRBBY"],["RBBYR","RBRBB","YBYRY","YYDBY","BBRRR"],["RBRBY","YBRYR","YRRDB","RYBYB","YBBRB"],["RBRRB","YBRYY","RYBBB","YBYBR","BRRDY"],["RBYBB","DRYRB","BYYBY","RRRYR","YRBBB"],["RBYBY","YBRRB","BRDRY","YBYBB","BRYRR"],["RBYRB","YDBBY","BYBYR","RYBRR","BRBYR"],["RBYRR","BYRBY","RYYBB","RBBRB","YYDRB"],["RBYRY","RRYBR","BRDYB","YBBRY","BRYBB"],["RBYYB","BYBYR","BRRRD","RBRRB","YYBBY"],["RBYYR","YBRBR","BRYDR","BBBYB","RRYYB"],["RRBRY","BRYYB","BBBYB","BYRYR","RDBRY"],["RRRBB","YBDYY","YRYBY","BBRBR","RYBBR"],["RRYBB","BRDBB","RYBRY","YRRRY","BBBYY"],["RRYRB","BBYBY","YRDRB","BRRBY","YBYBR"],["RYBBB","RBBRR","YRYBD","BYYBY","RBRRY"],["RYBBB","YRBBY","RRRYR","BBBYB","YRRDY"],["RYBBR","YRBRY","BRBYR","YBRBB","YDBYR"],["RYBDY","BBRBY","RYBRB","YRBRY","RBBYR"],["RYBRB","RRBYR","RYBYB","YBBDY","BRYBR"],["RYBYD","BBRRB","RBYBY","BRBYY","YRRRB"],["RYDYB","YRBRR","YBRBR","BYBBR","YRYBB"],["RYRBR","BRYBY","BBBRB","YRRBD","RYBYY"],["RYRBY","YBBBB","BRRYR","DYBRR","YBRYB"],["YBBRB","BYRYR","RBBBR","YRRYY","DYBRB"],["YBBRY","RYYYR","BYBRB","RRBYD","RBBBR"],["YBBYY","BRRBR","DRRRB","RYBYB","BYYBR"],["YBRBB","RYYDR","RYBBY","RRBYR","BBYRB"],["YBRBR","RRBBY","RBYRB","RYBRY","BYYBD"],["YBRRB","BBYRY","RBRBR","YBRYB","RYBDY"],["YBRRY","BBBDB","YRYYR","YRRRY","BBBBR"],["YBRYR","RBRRY","RBRBB","DYYBB","YBRYB"],["YBRYR","RDYRB","RBYRB","BBRRB","YBYYB"],["YBYBR","BRRBR","YRDYY","BBRBR","RYBYB"],["YBYYR","RYBRY","YBRBD","BBBRY","BRRRB"],["YDBYR","BYRBY","RBRBR","YRYBB","BRRBY"],["YDRRB","RBYBY","BBBYR","YYRBY","BRRBR"],["YDRRY","BYBBB","RYRRR","YBBRY","BBBYR"],["YRBBB","BYDYR","RRRBB","BBRYB","RYYRY"],["YRBBR","YBRYB","BRRBY","BRRYY","YBDRB"],["YRBDR","RRYRB","BRYYY","BYBRB","BRYBB"],["YRBDR","RYRYB","BYBBB","BYYRB","YRBRR"],["YRBYB","RBYRB","YYDBY","BRRBR","RRBYB"],["YRRBR","YBYYB","DBYRY","RRBBR","BBBYR"],["YRYYR","BYRBB","BBRRR","RYDYB","BBBRY"],["YYBBB","YRRRY","YRBYR","BBDRB","BBYRR"],["YYBYR","DBRRY","BRBBB","YBYRB","RBRYR"]];
+var config = configurationsR;
+
+var colors = ["","R","B","Y","D"];
+var classes = ["","red","blue","yellow","black"];
+var board = [
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0]
+];
+
+function findActive() {
+  var active = [];
+  for(var k=0;k<config.length;k++) {
+    var a = 1;
+    for(var i=0;i<5;i++) {
+      for(var j=0;j<5;j++) {
+        if(!board[i][j]) continue;
+        if(colors[ board[i][j] ] != config[k][i].charAt(j)) a = 0;
+      }
+    }
+    active.push(a);
+  }
+  return active;
+}
+
+function refresh() {
+  var active = findActive();
+  for(var i=0;i<5;i++) {
+    for(var j=0;j<5;j++) {
+      var id = "#"+i+""+j;
+      $(id).html("");
+      var counts = {"R":0,"B":0,"Y":0,"D":0};
+      for(var k=0;k<config.length;k++) {
+        if(!active[k]) continue;
+        counts[ config[k][i].charAt(j) ]++;
+      }
+
+      if(board[i][j]) {
+        $(id).attr("class",classes[ board[i][j] ]);
+      } else {
+        if(counts["R"] && counts["B"]+counts["Y"]+counts["D"]==0) {
+          $(id).attr("class","red guess");
+        }
+        else if(counts["B"] && counts["R"]+counts["Y"]+counts["D"]==0) {
+          $(id).attr("class","blue guess");          
+        }
+        else if(counts["Y"] && counts["B"]+counts["R"]+counts["D"]==0) {
+          $(id).attr("class","yellow guess");
+        }
+        else if(counts["D"] && counts["B"]+counts["R"]+counts["Y"]==0) {
+          $(id).attr("class","black guess");
+        }
+        else {
+          $(id).attr("class","");
+          $(id).html(
+            "<span class='cnt rd'>"+counts["R"]+"</span> "+
+            "<span class='cnt bl'>"+counts["B"]+"</span> "+
+            "<span class='cnt yl'>"+counts["Y"]+"</span> "+
+            "<span class='cnt bk'>"+counts["D"]+"</span> "
+          );
+        } 
+      }
+    }
+  }
+}
+
+$(function() {
+  refresh();
+  $(".row > div").click(function() {
+    var id = $(this).attr("id");
+    var i = id.charAt(id.length-2) | 0;
+    var j = id.charAt(id.length-1) | 0;
+    board[i][j] = (board[i][j]+1)%5;
+    refresh();
+  });
+  $("input").change(function() {
+    if(this.checked) config = configurationsB;
+    else config = configurationsR;
+    refresh();
+  });
+});
